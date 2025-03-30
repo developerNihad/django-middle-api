62942820d3fa3efd67fc075b0476263d9f8abd3a
Nihad Nahmatli <140307533+developerNihad@users.noreply.github.com>
2025-03-30 16:44:54 +0000

    commited

--- news/models.py
+++ news/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 class NewsThree(models.Model):
 
-    title = models.CharField(max_length=200),
+    title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.URLField()
+
+    class Meta:
+        db_table = 'newsthree'
 
     def __str__(self):
         return self.title
